c981cd5e4e839e92753431e5baa2d4d6d9096b85
Jim Nelson <jim@midnightfreddie.com>
2017-03-15 22:53:24 -0500

    reading tag type and name fields

--- nbt2json.go
+++ nbt2json.go
@@ -17,9 +17,26 @@ type NbtTag struct {
 func Nbt2Json(b []byte, byteOrder binary.ByteOrder) ([]byte, error) {
 	var data NbtTag
 	buf := bytes.NewReader(b[:])
-	err := binary.Read(buf, binary.LittleEndian, &data.TagType)
+	err := binary.Read(buf, byteOrder, &data.TagType)
 	if err != nil {
 		return nil, err
+	}
+	switch data.TagType {
+	case 0:
+		//do nothing
+	default:
+		var err error
+		var nameLen int16
+		err = binary.Read(buf, byteOrder, &nameLen)
+		if err != nil {
+			return nil, err
+		}
+		name := make([]byte, nameLen)
+		err = binary.Read(buf, byteOrder, &name)
+		if err != nil {
+			return nil, err
+		}
+		data.Name = string(name[:])
 	}
 	outJson, err := json.Marshal(data)
 	return outJson, nil
