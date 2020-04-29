f18d4ac7144f61014c0e60047e0f7f434a3aa989
Jim Nelson <jim@jimnelson.us>
2020-04-28 20:57:19 -0500

    stubs and comments including comment to .h discovery

--- cmd/libnbt2json/main.go
+++ cmd/libnbt2json/main.go
@@ -5,9 +5,29 @@ import (
 	"fmt"
 )
 
+// HelloDll is here as a test while I work out parameter passing
+// Any functions or vars exposed in the shared lib must be capitalized (Go rule)
+// The export comment is needed to have the "C" package make the item available
+//   in the shared library. Note there must be no space between the // and
+//   'export'
 //export HelloDll
 func HelloDll() {
 	fmt.Println("Hello from the libnbt2json dll!")
+}
+
+// NOTE: Functions don't do anything yet; I'm just trying to figure out how
+//   to pass C-native values from Go
+// Oh cool, these comments are in the .h file when no blank lines separate them
+//export Nbt2Json
+func Nbt2Json(b []byte) string {
+	return string(b)
+}
+
+// NOTE: Functions don't do anything yet; I'm just trying to figure out how
+//   to pass C-native values from Go
+//export Json2Nbt
+func Json2Nbt(s string) []byte {
+	return []byte(s)
 }
 
 func main() {}
--- examples/sharedlib/powershell.ps1
+++ examples/sharedlib/powershell.ps1
@@ -1,3 +1,15 @@
+<#
+    Powershell can use P/Invoke to load non-.NET DLL files
+
+    Place the DLL in the executable search path (the local folder is the easiest
+    option)
+
+    A .NET signature must be defined. It does not use the .h file, but you can
+    look at the .h file for guidance. "public" must be added in the case of
+    Powershell as we want the functions to be available in the script which
+    is external to the type class.
+#>
+
 $Signature = @'
 [DllImport("libnbt2json.dll")]
 public static extern void HelloDll();
