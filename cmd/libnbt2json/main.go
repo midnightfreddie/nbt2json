35118d61a76b706ae93a3860d6abe6d989224d7a
Jim Nelson <jim@jimnelson.us>
2020-04-29 00:26:42 -0500

    First success of actual by-dll nbt conversion

--- cmd/libnbt2json/main.go
+++ cmd/libnbt2json/main.go
@@ -4,6 +4,8 @@ import (
 	"C"
 	"fmt"
 	"unsafe"
+
+	"github.com/midnightfreddie/nbt2json"
 )
 
 // HelloDll is here as a test while I work out parameter passing
@@ -17,19 +19,26 @@ func HelloDll() {
 }
 
 // NOTE: Functions don't do anything yet; I'm just trying to figure out how
-//   to pass C-native values from Go
+//   to pass C-native values to/from Go
 // Oh cool, these comments are in the .h file when no blank lines separate them
+// The NBT data must be in a byte array. Pass a pointer to the array and the
+//   length of the array
+// Temporarily hard-codeed for Bedrock / little-endian only
 //export Nbt2Json
 func Nbt2Json(byteArray unsafe.Pointer, length C.int) *C.char {
 	var goByteArray = C.GoBytes(byteArray, length)
 	fmt.Print("The first byte in the byte array is ")
 	fmt.Println(goByteArray[0])
-	var tempString = "Hello from a Go string"
-	return C.CString(tempString)
+	jsonData, err := nbt2json.Nbt2Json(goByteArray, nbt2json.Bedrock, "")
+	if err != nil {
+		panic(err)
+	}
+	// var tempString = "Hello from a Go string"
+	return C.CString(string(jsonData))
 }
 
 // NOTE: Functions don't do anything yet; I'm just trying to figure out how
-//   to pass C-native values from Go
+//   to pass C-native values to/from Go
 //export Json2Nbt
 func Json2Nbt(cString *C.char) {
 	var s string
