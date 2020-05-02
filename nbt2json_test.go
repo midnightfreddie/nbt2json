4b71baf4a5d26d57e75e83926906d6697271a858
Jim Nelson <jim@jimnelson.us>
2020-05-02 02:00:01 -0500

    Added more test json tags

--- nbt2json_test.go
+++ nbt2json_test.go
@@ -8,7 +8,7 @@ import (
 
 // NOTE: Only testing round-trips for consistency, but errors will still fail the test.
 
-// TODO: More thorough test json
+// TODO: Add list/array types to test json
 
 var testJson = `{
   "nbt": [
@@ -17,14 +17,72 @@ var testJson = `{
       "name": "",
       "value": [
         {
+          "tagType": 1,
+          "name": "TestByte",
+          "value": 64
+        },
+        {
           "tagType": 2,
-          "name": "Test",
-          "value": 256
+          "name": "TestShort",
+          "value": 32767
+        },
+        {
+          "tagType": 3,
+          "name": "TestInt",
+          "value": 2147483647
+        },
+        {
+          "tagType": 4,
+          "name": "TestLong",
+          "value": 9223372036854775807
+        },
+        {
+          "tagType": 5,
+          "name": "TestFloat",
+          "value": 1.234567e+38
+        },
+        {
+          "tagType": 6,
+          "name": "TestDouble",
+          "value": 1.23456789012345e+307
+        },
+        {
+          "tagType": 8,
+          "name": "TestString",
+          "value": "This is a test string"
+        },
+        {
+          "tagType": 0,
+          "name": "",
+          "value": null
         }
       ]
     }
   ]
 }`
+
+/*
+        {
+          "tagType": 7,
+          "name": "TestByteArray",
+          "value": 256
+        },
+				{
+					"tagType": 9,
+					"name": "TestList",
+					"value": 256
+				},
+				{
+					"tagType": 11,
+					"name": "TestIntArray",
+					"value": 256
+				},
+				{
+					"tagType": 12,
+					"name": "TestByteArray",
+					"value": 256
+				},
+*/
 
 func TestRoundTrip(t *testing.T) {
 	h := sha1.New()
